54e7869765de930129d78bae0336bfc2f1eff121
Harshwardhan <harsh28123@gmail.com>
2026-05-07 15:06:09 +0530

    making use of http package to create a server

--- Practice.go
+++ Practice.go
@@ -8,21 +8,21 @@ import (
 	"strings"
 )
 
-func main() {
+// func main() {
 
-	ln, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		// handle error
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			// handle error
-		}
-		go handleConnection(conn)
-	}
+// 	ln, err := net.Listen("tcp", ":8080")
+// 	if err != nil {
+// 		// handle error
+// 	}
+// 	for {
+// 		conn, err := ln.Accept()
+// 		if err != nil {
+// 			// handle error
+// 		}
+// 		go handleConnection(conn)
+// 	}
 
-}
+// }
 
 func handleConnection(conn net.Conn) {
 
---
+++ httpServer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+type server struct {
+	addr string
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the server"))
+}
+
+func main() {
+	s := &server{":8080"}
+	http.ListenAndServe(s.addr, s)
+}
